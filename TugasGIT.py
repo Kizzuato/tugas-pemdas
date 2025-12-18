d83ade15f8913142e14a70fd01bd50c76c0b12d3
Kizzuato <kyyajuara@gmail.com>
2025-12-19 00:13:34 +0700

    Menambahkan program pengolahan data panen

--- TugasGIT.py
+++ TugasGIT.py
@@ -53,28 +53,24 @@ jumlah_jagung = data_panen['lokasi2']['hasil_panen']['jagung']
 print(f"\nJumlah Hasil Panen Jagung di Lokasi 2: {jumlah_jagung} kg")
 
 # Tampilkan nama lokasi dari lokasi3.
-nama_lokasi = data_panen['lokasi3']['nama_lokasi']
-print(f"\nNama Lokasi 3: {nama_lokasi}")
+padi = {}
+kedelai = {}
 
-# Masukkan Jumlah Hasil Panen Padi dan Kedelai Setiap Lokasi ke Dalam Variabel yang Berbeda:
-padi_lokasi1 = data_panen['lokasi1']['hasil_panen']['padi']
-kedelai_lokasi1 = data_panen['lokasi1']['hasil_panen']['kedelai']
-padi_lokasi2 = data_panen['lokasi2']['hasil_panen']['padi']
-kedelai_lokasi2 = data_panen['lokasi2']['hasil_panen']['kedelai']
-padi_lokasi3 = data_panen['lokasi3']['hasil_panen']['padi']
-kedelai_lokasi3 = data_panen['lokasi3']['hasil_panen']['kedelai']
-padi_lokasi4 = data_panen['lokasi4']['hasil_panen']['padi']
-kedelai_lokasi4 = data_panen['lokasi4']['hasil_panen']['kedelai']
-padi_lokasi5 = data_panen['lokasi5']['hasil_panen']['padi']
-kedelai_lokasi5 = data_panen['lokasi5']['hasil_panen']['kedelai']
+for key, lokasi in data_panen.items():
+    padi[key] = lokasi['hasil_panen']['padi']
+    kedelai[key] = lokasi['hasil_panen']['kedelai']
 
-print(f"\nJumlah Hasil Panen Padi di Lokasi 1: {padi_lokasi1} kg")
-print(f"Jumlah Hasil Panen Kedelai di Lokasi 1: {kedelai_lokasi1} kg")
-print(f"\nJumlah Hasil Panen Padi di Lokasi 2: {padi_lokasi2} kg")
-print(f"Jumlah Hasil Panen Kedelai di Lokasi 2: {kedelai_lokasi2} kg")
-print(f"\nJumlah Hasil Panen Padi di Lokasi 3: {padi_lokasi3} kg")
-print(f"Jumlah Hasil Panen Kedelai di Lokasi 3: {kedelai_lokasi3} kg")
-print(f"\nJumlah Hasil Panen Padi di Lokasi 4: {padi_lokasi4} kg")
-print(f"Jumlah Hasil Panen Kedelai di Lokasi 4: {kedelai_lokasi4} kg")
-print(f"\nJumlah Hasil Panen Padi di Lokasi 5: {padi_lokasi5} kg")
-print(f"Jumlah Hasil Panen Kedelai di Lokasi 5: {kedelai_lokasi5} kg")
+print("Data Padi:", padi)
+print("Data Kedelai:", kedelai)
+
+# Buat Percabangan
+for data in data_panen.values():
+    padi = data['hasil_panen']['padi']
+    jagung = data['hasil_panen']['jagung']
+    nama = data['nama_lokasi']
+
+    if padi > 1300 or jagung > 800:
+        print(f"{nama} : Perlu perhatian khusus")
+    else:
+        print(f"{nama} : Kondisi baik")
+
